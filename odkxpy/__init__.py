c557c3a63ad48babf58dcb45293aa14c4956f692
Frank Dekervel <frank@kapernikov.com>
2019-07-11 12:51:10 +0200

    LocalSyncMode in odkxpy scope

--- odkxpy/__init__.py
+++ odkxpy/__init__.py
@@ -1,4 +1,4 @@
 from .odkx_connection import OdkxConnection
 from .odkx_server_meta import OdkxServerMeta
-from .odkx_local_table import OdkxLocalTable
+from .odkx_local_table import OdkxLocalTable, LocalSyncMode
 from .local_storage_sql import SqlLocalStorage
